58e442e6662b038729c20a7638dad7025356ed1a
Saurabh Kumar Singh <73352093+Saurabh7Goku@users.noreply.github.com>
2023-12-29 18:22:10 +0530

    Update app.py from violence_detection dir

--- app.py
+++ app.py
@@ -1,178 +1,129 @@
-import cv2
-from flask import Flask, render_template, Response, request, jsonify
-import numpy as np
-from skimage.transform import resize
+from flask import Flask, render_template, Response, request
+import socketio
 import tensorflow as tf
-from fun.invisibleMinds import pred_fight, videoFightModel2
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, FileField
+from wtforms.validators import DataRequired
+import cv2
+import numpy as np
+import imageio
+from functions.invisibleMinds import pred_fight, videoFightModel3
 
 app = Flask(__name__)
-model = videoFightModel2(tf, wight='invisibleminds.hdfs')
-
-cap = None
-video_file_path = None
-url = None
+app.config['SECRET_KEY'] = '4cf659de047f5ec423725d1574c942e9'
 violence_detections = []
 
-def initialize_video_capture(file_path):
-    global cap
-    cap = cv2.VideoCapture(file_path)
+# Your model loading code here
+model = videoFightModel3(tf, wight='invisibleMinds.hdfs')
 
-def process_frame(frame, frames):
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    frm = resize(frame, (160, 160, 3))
-    frm = np.expand_dims(frm, axis=0)
-    if np.max(frm) > 1:
-        frm = frm / 255.
+class VideoForm(FlaskForm):
+    video_url = StringField('Video URL')
+    video_file = FileField('Upload Video File')
+    submit = SubmitField('Start Analysis')
 
-    ysdatav2 = np.zeros((1, 30, 160, 160, 3), dtype=np.float16)
-    ysdatav2[0][:][:] = frames
-    predaction = pred_fight(model, ysdatav2, acuracy=0.96)
 
-    if predaction[0]:
-        cv2.putText(frame,
-                    'Violence Detected!!!',
-                    (100, 200),
-                    font, 3,
-                    (238, 75, 43),
-                    5,
-                    cv2.LINE_4)
 
-        # Store violence detection information
-        detection_info = {
-            'timestamp': str(cap.get(cv2.CAP_PROP_POS_MSEC)),
-            'video_url': f'/videos/output-{len(violence_detections)}.mp4'
-        }
-        violence_detections.append(detection_info)
+def resize(frame, size=(160, 160)):
+    # Resize the frame to the specified size
+    return cv2.resize(frame, size)
 
-        # Update the sidebar dynamically
-        update_sidebar()
+def gen(video_source, model):
+    vid = imageio.get_reader(video_source, 'ffmpeg')
 
-    return frame
-
-def update_sidebar():
-    global violence_detections
-    data = {
-        'violence_detections': violence_detections
-    }
-    # Broadcast the updated data to all connected clients
-    socketio.emit('update_sidebar', data)
-
-def gen():
-    global cap, violence_detections
     i = 0
     frames = np.zeros((30, 160, 160, 3), dtype=np.float16)
     old = []
     j = 0
 
-    while cap is not None and cap.isOpened():
+    for frame in vid.iter_data():
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)  # Convert imageio frame to OpenCV format
+
         font = cv2.FONT_HERSHEY_SIMPLEX
-        ret, frame = cap.read()
-        if ret:
-            # Process every third frame
-            if i % 3 == 0:
-                processed_frame = process_frame(frame.copy(), frames)
-                ret, jpeg = cv2.imencode('.jpg', processed_frame)
-                if ret:
-                    yield (b'--frame\r\n'
-                           b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n')
+        if i > 29:
+            ysdatav2 = np.zeros((1, 30, 160, 160, 3), dtype=np.float16)
+            ysdatav2[0][:][:] = frames
+            predaction = pred_fight(model, ysdatav2, acuracy=0.96)
+            if predaction[0]:
+                fourcc = cv2.VideoWriter_fourcc(*'XVID')
+                vio = cv2.VideoWriter("./videos/output-"+str(j)+".avi", fourcc, 10.0, (frame.shape[1], frame.shape[0]))
+                print('Violence detected here ...')
+                print("timestamp is: ", i * vid.get_meta_data()['duration'] / len(vid))
+                for x in old:
+                    vio.write(x)
+                vio.release()
+                cv2.putText(frame, 
+                            'Violence Detected!!!', 
+                            (100, 200), 
+                            font, 3, 
+                            (238, 75, 43), 
+                            5, 
+                            cv2.LINE_4)
+                 # Store violence detection information
+                # detection_info = {
+                #     'timestamp': str(cap.get(cv2.CAP_PROP_POS_MSEC)),
+                #     'video_url': f'/videos/output-{len(violence_detections)}.mp4'
+                # }
+                # violence_detections.append(detection_info)
 
-            if i > 29:
-                ysdatav2 = np.zeros((1, 30, 160, 160, 3), dtype=np.float16)
-                ysdatav2[0][:][:] = frames
-                predaction = pred_fight(model, ysdatav2, acuracy=0.96)
-                if predaction[0]:
-                    cv2.imshow('video', frame)
-                    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-                    vio = cv2.VideoWriter(f"./videos/output-{j}.avi", fourcc, 10.0, (frame.shape[1], frame.shape[0]))
-                    vio = cv2.VideoWriter(f"./videos/output-{j}.mp4", cv2.VideoWriter_fourcc(*'mp4v'), 10,
-                                          (frame.shape[1], frame.shape[0]))
-                    print('Violence detected here ...')
-                    print("timestamp is: ", str(cap.get(cv2.CAP_PROP_POS_MSEC)))
-                    for x in old:
-                        vio.write(x)
-                    vio.release
-                    cv2.putText(frame,
-                                'Violence Detected!!!',
-                                (100, 200),
-                                font, 3,
-                                (238, 75, 43),
-                                5,
-                                cv2.LINE_4)
-
-                    # Store violence detection information
-                    detection_info = {
-                        'timestamp': str(cap.get(cv2.CAP_PROP_POS_MSEC)),
-                        'video_url': f'/videos/output-{j}.mp4'
-                    }
-                    violence_detections.append(detection_info)
-
-                i = 0
-                j += 1
-                frames = np.zeros((30, 160, 160, 3), dtype=np.float16)
-                old = []
-            else:
-                frm = resize(frame, (160, 160, 3))
-                old.append(frame)
-                fshape = frame.shape
-                fheight = fshape[0]
-                fwidth = fshape[1]
-                frm = np.expand_dims(frm, axis=0)
-                if np.max(frm) > 1:
-                    frm = frm / 255.
-                frames[i][:] = frm
-                i += 1
+                # Update the sidebar dynamically
+                # update_sidebar()        
+            i = 0
+            j += 1
+            frames = np.zeros((30, 160, 160, 3), dtype=np.float16)
+            old = []
         else:
-            break
+            frm = resize(frame, (160, 160))
+            old.append(frame)
+            frm = np.expand_dims(frm, axis=0)
+            if np.max(frm) > 1:
+                frm = frm / 255.
+            frames[i][:] = frm
+            i += 1
 
-    cap.release()
+        frame = cv2.imencode('.jpg', frame)[1].tobytes()
+        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-@app.route('/')
+    vid.close()
+
+# def update_sidebar():
+#     global violence_detections
+#     data = {
+#         'violence_detections': violence_detections
+#     }
+#     # Broadcast the updated data to all connected clients
+#     socketio.emit('update_sidebar', data)
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    """Video streaming home page."""
-    return render_template('index.html', violence_detections=violence_detections)
+    form = VideoForm()
+    if form.validate_on_submit():
+        video_url = form.video_url.data
+        video_file = form.video_file.data
+        if video_url:
+            video_source = video_url
+        elif video_file:
+            video_source = save_uploaded_file(video_file)
+        else:
+            # Handle case where neither URL nor file is provided
+            return render_template('index.html', form=form)
 
-@app.route('/process_video', methods=['POST'])
-def process_video():
-    global video_file_path, violence_detections, cap
-    video_file = request.files.get('video_file')
-    video_url = request.form.get('video_url')
+        return render_template('index.html', form=form, video_source=video_source)
 
-    # Clear the sidebar when selecting a new video
-    violence_detections = []
-
-    if video_file:
-        video_file_path = video_file.filename
-        video_file.save(video_file_path)
-        initialize_video_capture(video_file_path)
-
-    elif video_url:
-        url = video_url
-        # video_url.save(url)
-        initialize_video_capture(url)
-
-    return render_template('index.html', violence_detections=violence_detections)
-
-@app.route('/end_video', methods=['POST'])
-def end_video():
-    global cap
-    if cap is not None:
-        cap.release()
-    return jsonify(success=True)
+    return render_template('index.html', form=form)
 
 @app.route('/video_feed')
 def video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    video_source = request.args.get('video_source', 0)
+    return Response(gen(video_source, model), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-if __name__ == "__main__":
-    from flask_socketio import SocketIO
+def save_uploaded_file(file):
+    # Save the uploaded file and return the file path
+    # You might want to save it in a folder and return the path
+    # For simplicity, this example saves the file in the current working directory
+    file_path = file.filename
+    file.save(file_path)
+    return file_path
 
-    socketio = SocketIO(app)
-
-    @socketio.on('disconnect')
-    def handle_disconnect():
-        global cap
-        if cap is not None:
-            cap.release()
-
-    socketio.run(app, debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
